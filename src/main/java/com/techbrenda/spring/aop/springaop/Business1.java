2a2af76067f1202c1f6735c2578a2dfd011d5b0b
TechBrenda <tech.brenda@gmail.com>
2019-03-03 16:36:31 -0600

    Set up example

---
+++ src/main/java/com/techbrenda/spring/aop/springaop/Business1.java
@@ -0,0 +1,16 @@
+package com.techbrenda.spring.aop.springaop;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Business1 {
+
+	@Autowired
+	private Dao1 dao1;
+	
+	public String calculateSomething() {
+		// Business Logic
+		return dao1.retrieveSomething();
+	}
+}
