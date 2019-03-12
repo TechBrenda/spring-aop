7832b8396ec96ef8677d1fbc5e6cc69c3b305a6e
TechBrenda <tech.brenda@gmail.com>
2019-03-12 17:04:50 -0500

    Finished Before Annotation Class

--- src/main/java/com/techbrenda/spring/aop/springaop/aspect/UserAccessAspect.java
+++ src/main/java/com/techbrenda/spring/aop/springaop/aspect/UserAccessAspect.java
@@ -1,5 +1,6 @@
 package com.techbrenda.spring.aop.springaop.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -8,14 +9,15 @@ import org.springframework.context.annotation.Configuration;
 
 @Aspect
 @Configuration
-public class BeforeAspect {
+public class UserAccessAspect {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	//execution(* PACKAGE.*.*(..))
 	
 	@Before("execution(* com.techbrenda.spring.aop.springaop.business.*.*(..))")
-	public void before() {
-		logger.info("Intercepted Method Calls {}");
+	public void before(JoinPoint joinPoint) {
+		logger.info("Check for user access");
+		logger.info("Allowed execution for {}", joinPoint);
 	}
 }
